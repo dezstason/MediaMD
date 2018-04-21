89d30bfa3ab4c5216566b5bb649f11e8db12f62a
Stanislav <gutalov97@gmail.com>
2018-04-21 12:31:06 +0300

    Added tabs on site + dialog window

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,48 +1,58 @@
 <template>
   <div id="app">
+
+    <!-- NAVBAR OF THE SITE -->
+
     <div class="navbar">
       <md-menu class="topItem">
-        <md-button md-menu-trigger>Default</md-button>
+        <md-button md-menu-trigger>My Account</md-button>
 
         <md-menu-content class="item">
-          <md-menu-item>My Item 1</md-menu-item>
-          <md-menu-item>My Item 2</md-menu-item>
-          <md-menu-item>My Item 3</md-menu-item>
+          <md-menu-item>Register</md-menu-item>
+          <md-menu-item>Log In</md-menu-item>
         </md-menu-content>
       </md-menu>
+
       <md-menu class="topItem">
-        <md-button md-menu-trigger>Default</md-button>
-
-        <md-menu-content class="item">
-          <md-menu-item>My Item 1</md-menu-item>
-          <md-menu-item>My Item 2</md-menu-item>
-          <md-menu-item>My Item 3</md-menu-item>
-
-          <md-menu-item>My Item 4</md-menu-item>
+        <md-button md-menu-trigger>
+          <i class="badge" :md-active.sync = "visib" v-if="newPosts">{{ newPosts }}</i>Cart</md-button>
+        <md-menu-content class="item" >
+          <md-menu-item>Your Cart</md-menu-item>
+          <md-menu-item><hr style="width: 100%"></md-menu-item>
+          <md-menu-item v-for="order in orders">{{ order.title }}        {{ order.price}}$</md-menu-item>
         </md-menu-content>
       </md-menu>
     </div>
 
+    <!-- TABS WITH CARDS AND OTHER INFORMATION -->
 
-    <md-card class="card" md-with-hover v-for="card in cards">
-      <md-card-media>
-        <img v-bind:src="card.urlImg">
-      </md-card-media>
+    <md-tabs md-sync-route>
+      <md-tab id="tab-home" md-label="Home" to="/components/tabs/home" class="tabs">
+        <md-card class="card" md-with-hover v-for="card in cards">
+          <md-card-media>
+            <img v-bind:src="card.urlImg">
+          </md-card-media>
 
-      <md-card-header>
-        <div class="md-title">{{ card.title }}</div>
-        <div class="md-subhead">Subtitle here</div>
-      </md-card-header>
+          <md-card-header>
+            <div class="md-title">{{ card.title }}</div>
+            <div class="md-subhead">{{ card.price }} $</div>
+          </md-card-header>
 
-      <md-card-actions>
-        <md-button class="md-accent" @click="first = true">Buy </md-button>
-        <md-button>View More</md-button>
-      </md-card-actions>
-    </md-card>
+          <md-card-actions>
+            <md-button class="md-accent" @click="first = true; newPosts += 1; visib = true; orderAdd(card.title, card.price)" md-ripple>Buy </md-button>
+            <md-button class="md-accent" @click="second = true">View More</md-button>
+          </md-card-actions>
+        </md-card>
+      </md-tab>
 
+      <md-tab id="tab-pages" md-label="Pages" to="/components/tabs/pages">
+        Pages tab
+        <p>Unde provident nemo reiciendis officia, possimus repellendus. Facere dignissimos dicta quis rem. Aliquam aspernatur dolor atque nisi id deserunt laudantium quam repellat.</p>
+      </md-tab>
 
+    </md-tabs>
 
-
+    <!-- LEFT BOTTOM CORNER BUTTONS -->
 
     <div class="actionButtons">
       <md-button class="md-fab"  style="background-color: cornflowerblue; color: white;">
@@ -66,9 +76,16 @@
       </md-button>
     </div>
 
+    <!-- DIALOG WINDOW -->
 
-
+    <md-dialog-alert
+      :md-active.sync="first"
+      md-content="Your order was added to cart"
+      md-confirm-text="Cool!"
+      class="dialogWindow"
+    />
   </div>
+
 </template>
 
 <script>
@@ -82,34 +99,49 @@ export default {
       cards: [
         {
           'title': 'iPhone 6',
-          'urlImg': 'https://assets.pcmag.com/media/images/441875-iphone-6-inline-2.jpg?thumb=y&width=980&height=552'
+          'urlImg': 'https://assets.pcmag.com/media/images/441875-iphone-6-inline-2.jpg?thumb=y&width=980&height=552',
+          'price' : 700
         },
         {
           'title': 'iPhone 5',
-          'urlImg': 'https://p1.akcdn.net/full/217009883.apple-iphone-5s-16gb.jpg'
+          'urlImg': 'https://p1.akcdn.net/full/217009883.apple-iphone-5s-16gb.jpg',
+          'price' : 700
         },
         {
           'title': 'Samsung S9',
-          'urlImg': 'https://p1.akcdn.net/full/217009883.apple-iphone-5s-16gb.jpg'
+          'urlImg': 'https://htstatic.imgsmail.ru/pic_image/126ac6cb7c7b167974345444bed93d37/410/329/1252342/',
+          'price' : 850
         },
         {
           'title': 'Xiaomi Redmi 5',
-          'urlImg': 'https://p1.akcdn.net/full/217009883.apple-iphone-5s-16gb.jpg'
+          'urlImg': 'https://d2giyh01gjb6fi.cloudfront.net/default/0002/15/thumb_114133_default_big.jpeg',
+          'price' : 550
         },
         {
-          'title': 'iPhone 6',
-          'urlImg': 'https://p1.akcdn.net/full/217009883.apple-iphone-5s-16gb.jpg'
+          'title': 'iPhone X',
+          'urlImg': 'https://www.vodafone.co.uk/cs/groups/public/documents/webcontent/vfcon084827.jpg',
+          'price' : 1089
         },
         {
-          'title': 'iPhone 5',
-          'urlImg': 'https://p1.akcdn.net/full/217009883.apple-iphone-5s-16gb.jpg'
+          'title': 'Samsung Note 8',
+          'urlImg': 'https://3dnews.ru/assets/external/illustrations/2017/08/03/956423/sg1.jpg',
+          'price' : 950
         },
         {
-          'title': 'Samsung S9',
-          'urlImg': 'https://p1.akcdn.net/full/217009883.apple-iphone-5s-16gb.jpg'
+          'title': 'iPhone 8',
+          'urlImg': 'https://images.kaina24.lt/43/61/apple-iphone-8-256gb-3.jpg',
+          'price' : 900
         }
       ],
-      first: false
+      first: false,
+      visib: false,
+      newPosts: 0,
+      orders: []
+    }
+  },
+  methods: {
+    orderAdd: function (msg, prc) {
+      this.orders.push({title: msg, price: prc})
     }
   }
 
@@ -132,15 +164,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   padding: 20px;
-  width: 1000px;
+  width: 1030px;
   margin: 0 auto;
-}
 
+}
+body{
+  padding-bottom: 10px;
+  /*background-color: #f7f8f9;*/
+  background: url("https://5dwallpaper.com/wp-content/uploads/2016/09/white-brick-wallpaper7.jpg") no-repeat fixed;
+}
 .card{
     width: 300px;
     float: left;
     margin-left: 15px;
     margin-top: 20px;
+    background-color: white;
 }
 
 .card img {
@@ -152,9 +190,13 @@ export default {
   height: 200px;
   font-size: 25px;
 }
+  .tabs{
+    font-size: 45px;
+  }
+
   .navbar{
     height: 80px;
-    margin-top: 20px;
+    background-color: white;
     margin-bottom: 10px;
     box-shadow: 0 3px 1px -2px rgba(0,0,0,.2),0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12);
     padding: 23px;
@@ -178,4 +220,24 @@ export default {
   .md-button{
     margin-left: 0px;
   }
+
+
+.badge {
+  width: 22px;
+  height: 22px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: -10px;
+  right: -25px;
+  background: red;
+  border-radius: 100%;
+  color: white;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 600;
+  letter-spacing: -.05em;
+  font-family: 'Roboto Mono', monospace;
+}
 </style>
